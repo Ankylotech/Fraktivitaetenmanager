b42c81e3c1cd53b54ba237301f535866014a5e7d
Ankylotech <jonas.rubeck@gmail.com.com>
2025-11-02 18:46:02 +0100

    bambambam

--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,136 @@
-use std::{sync::{mpsc,Arc,Mutex},thread};
+use chrono::NaiveDate;
+use uuid::Uuid;
 
-pub struct ThreadPool{
-    workers: Vec<Worker>,
-    sender: Option<mpsc::Sender<Job>>
+// Fields that should not matter are intentionaly removed
+#[derive(
+    Clone, Debug,
+)]
+pub struct FractivityRoom {
+    pub id: Uuid,
+    pub priority: i32,
 }
 
-type Job = Box<dyn FnOnce() + Send + 'static>;
-impl ThreadPool {
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
+#[derive(
+    Clone, Debug,PartialEq, Eq
+)]
+pub struct FractivityEntry {
+    pub id: Uuid,
+    pub instructor_extension_uuids: Vec<Uuid>,
+    pub duration: i32,
+    pub allowed_rooms: Vec<Uuid>,
+    pub allowed_starts: Vec<i32>,
+    pub preparation_time: i32,
+    pub follow_up_time: i32,
+    pub start_day: NaiveDate,
+}
 
-        let (sender, receiver) = mpsc::channel();
-
-        let receiver = Arc::new(Mutex::new(receiver));
-
-        let mut workers = Vec::with_capacity(size);
-
-        for id in 0..size {
-            workers.push(Worker::new(id, Arc::clone(&receiver)));
+pub fn distribute(
+    values: &mut Vec<(FractivityEntry, Option<(Uuid, i32)>)>, all_rooms: &Vec<FractivityRoom>
+) -> Result<Vec<(FractivityEntry, (Uuid, i32))>, FractivityEntry> {
+    let mut distribute_cur = values.len();
+    let mut max_len = -1;
+    for i in 0..values.len() {
+        if values[i].1.is_none() && values[i].0.allowed_starts.len() == 1 {
+            distribute_cur = i;
+            break;
         }
-
-        ThreadPool {workers, sender: Some(sender)}
-    }
-
-    pub fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
-    {
-        let job = Box::new(f);
-        self.sender.as_ref().unwrap().send(job).unwrap();
-    }
-}
-
-impl Drop for ThreadPool {
-    fn drop(&mut self) {
-        drop(self.sender.take());
-
-        for worker in &mut self.workers {
-            println!("Shutting down worker {}", worker.id);
-
-            if let Some(thread) = worker.thread.take() {
-                thread.join().unwrap();
-            }
+        if values[i].1.is_none() && values[i].0.duration > max_len {
+            distribute_cur = i;
+            max_len = values[i].0.duration;
         }
     }
-}
-
-struct Worker {
-    id: usize,
-    thread: Option<thread::JoinHandle<()>>,
-}
-
-impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move || loop {
-            let message = receiver.lock().unwrap().recv();
-
-            match message {
-                Ok(job) => {
-                    println!("Worker {} executing a job", id);
-                    job();
-                },
-                Err(_) => {
-                    println!("Worker {} disconnected. Shutting down", id);
-                    break;
+    if distribute_cur == values.len() {
+        return Ok(
+            values
+                .into_iter()
+                .map(|(e, d)| match d {
+                    Some(v) => (e.clone(), v.clone()),
+                    None => unreachable!(),
+                })
+                .collect(),
+        );
+    }
+    let mut last_end = 0;
+    let mut starts = values[distribute_cur].0.allowed_starts.clone();
+    let duration = values[distribute_cur].0.duration;
+    for start in &starts {
+        if *start + duration > last_end {
+            last_end = *start + duration;
+        }
+    }
+    let mut concurrent_fractivities = vec![0; (last_end as usize) + 1];
+    for v in values.iter() {
+        match v.1 {
+            Some((_, start)) => {
+                for i in 0..=v.0.duration {
+                    if ((start + i) as usize) < concurrent_fractivities.len() {
+                        concurrent_fractivities[(start + i) as usize] += 1;
+                    } else {
+                        break;
+                    }
                 }
             }
-
-        });
-
-        Self { thread: Some(thread), id}
+            None => (),
+        }
     }
+    let mut r: Vec<&FractivityRoom> = all_rooms.iter().filter(|r| values[distribute_cur].0.allowed_rooms.clone().contains(&r.id)).collect();
+    r.sort_by_key(|f| -f.priority);
+    let rooms: Vec<Uuid> = r.iter().map(|f| f.id).collect();
+    let prep = values[distribute_cur].0.preparation_time;
+    let follow = values[distribute_cur].0.follow_up_time;
+    let day = values[distribute_cur].0.start_day;
+    starts.sort_by_cached_key(|s| {
+        concurrent_fractivities[(*s as usize)..=((*s + duration) as usize)]
+            .iter()
+            .max()
+    });
+    let mut result = Err(values[distribute_cur].0.clone());
+    for start in starts {
+        if result.is_ok() {
+            break;
+        }
+        for room in &rooms {
+            let mut legal = true;
+            for v in values.iter() {
+                if day != v.0.start_day {
+                    continue;
+                }
+                match &v.1 {
+                    Some((dist_id, dist_start)) => {
+                        if (start - prep >= *dist_start - v.0.preparation_time
+                            && start - prep < *dist_start + v.0.duration + v.0.follow_up_time)
+                            || (*dist_start - v.0.preparation_time >= start - prep
+                                && *dist_start - v.0.preparation_time < start + duration + follow)
+                        {
+                            if *dist_id == *room {
+                                legal = false;
+                                break;
+                            }
+                            legal = v.0.instructor_extension_uuids.iter().all(|id| {
+                                !values[distribute_cur]
+                                    .0
+                                    .instructor_extension_uuids
+                                    .contains(id)
+                            });
+                            if !legal {
+                                break;
+                            }
+                        }
+                    }
+                    None => (),
+                }
+            }
+            if !legal {
+                continue;
+            }
+            let dist = (*room, start);
+            values[distribute_cur].1 = Some(dist);
+            result = distribute(values,all_rooms); 
+            if result.is_ok() {
+                return result;
+            }
+            values[distribute_cur].1 = None;
+        }
+    }
+    result
 }
